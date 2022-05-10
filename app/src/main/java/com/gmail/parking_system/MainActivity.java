acb48a138073582b38839fab0712e1bcab2fa161
shimwoojin <ggoggal@gmail.com>
2022-05-10 14:43:10 +0900

    add background png file to main_activity.xml change app name add button to activity_main

--- app/src/main/java/com/gmail/parking_system/MainActivity.java
+++ app/src/main/java/com/gmail/parking_system/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gmail.parking_system;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView1;
-    private Button send_button;
+    private Button send_button, pay_button;
     private Socket socket;
     private static String SERVER_IP = "192.168.0.5";
 
@@ -22,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         send_button = findViewById(R.id.send_button);
+        pay_button = findViewById(R.id.pay_button);
+
+        pay_button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Intent intent = new Intent( MainActivity.this, Show_pay_Activity.class );
+            startActivity( intent );
+            }
+        });
 
         send_button.setOnClickListener(new View.OnClickListener() {
 
